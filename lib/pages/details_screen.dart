cbf8c6c00dbdcd89aa0a56c183656d75b27aa452
Ahmed101887 <120632277+Ahmed101887@users.noreply.github.com>
2022-12-23 10:09:55 +0200

    .

--- lib/pages/details_screen.dart
+++ lib/pages/details_screen.dart
@@ -1,47 +1,45 @@
+// ignore_for_file: unused_element, prefer_const_constructors, unused_import
+
 import 'package:flutter/material.dart';
 import 'package:flutter_application/shared/colors.dart';
 
-class Details extends StatelessWidget {
+class Details extends StatefulWidget {
   const Details({super.key});
 
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          actions: [
-            Padding(
-              padding: const EdgeInsets.only(right: 12),
-              child: Row(
-                children: [
-                  Stack(
-                    children: [
-                      Container(
-                          child: Text(
-                            "4",
-                            style: const TextStyle(
-                                color: Color.fromARGB(255, 114, 3, 3)),
-                          ),
-                          padding: EdgeInsets.all(5),
-                          decoration: BoxDecoration(
-                              color: Color.fromARGB(211, 164, 255, 193),
-                              shape: BoxShape.circle)),
-                      IconButton(
-                          onPressed: () {},
-                          icon: Icon(Icons.add_shopping_cart)),
-                    ],
-                  ),
-                  Text("\$50000", style: const TextStyle(fontSize: 20)),
-                ],
-              ),
-            ),
-          ],
-          backgroundColor: appbarGreen,
-          title: Text("Home"),
-        ));
+      appBar: AppBar(
+        actions: [
+          Row(
+       
+        
+      ))
+  ],
+)
+            ],
+          )
+          
+        ],
+      ),
+
+    )
+
+
+  // ignore: dead_code
+  @override
+  State<Details> createState() =>
+_DetailsState ();
+
+  
   }
+
+  @override
+  dynamic noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
 }
 
-      body: Text("details")
-      
-}
+class _DetailsState extends State<Details> {
+  
+
+  @override
+  dynamic noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
 }
