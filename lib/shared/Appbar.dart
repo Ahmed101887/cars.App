c9930149ab3dece853e2b8d91c971c287fbfbef2
Ahmed101887 <120632277+Ahmed101887@users.noreply.github.com>
2022-12-24 09:45:22 +0200

    /

--- lib/shared/Appbar.dart
+++ lib/shared/Appbar.dart
@@ -2,3 +2,6 @@ import 'package:flower_app/pages/checkout.dart';
 import 'package:flower_app/provider/cart.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
+class ProductsAndPrice extends StatelessWidget {
+/
