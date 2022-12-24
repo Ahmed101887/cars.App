a851e463bab7369df689c6969143ebf5cf2f0a4f
Ahmed101887 <120632277+Ahmed101887@users.noreply.github.com>
2022-12-24 09:50:34 +0200

    ,

--- lib/shared/Appbar.dart
+++ lib/shared/Appbar.dart
@@ -20,3 +20,4 @@ class ProductsAndPrice extends StatelessWidget {
                     "${Carttt.selectedProducts.length}",
                     style: TextStyle(
                         fontSize: 16, color: Color.fromARGB(255, 0, 0, 0)),
+                  ),
