fc6de53d6247f458da9baab069deb48fb462cf51
Ahmed101887 <120632277+Ahmed101887@users.noreply.github.com>
2022-12-24 09:45:46 +0200

    .

--- lib/shared/Appbar.dart
+++ lib/shared/Appbar.dart
@@ -4,4 +4,5 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class ProductsAndPrice extends StatelessWidget {
-/
+  const ProductsAndPrice({Key? key}) : super(key: key);
+
