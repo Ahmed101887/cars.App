4c2781372bb624e671b24ee32c20f4dcd82bab1c
Ahmed101887 <120632277+Ahmed101887@users.noreply.github.com>
2022-12-24 09:52:57 +0200

    .

--- lib/shared/Appbar.dart
+++ lib/shared/Appbar.dart
@@ -25,3 +25,4 @@ class ProductsAndPrice extends StatelessWidget {
                   decoration: BoxDecoration(
                       color: Color.fromARGB(211, 164, 255, 193),
                       shape: BoxShape.circle)),
+            ),
