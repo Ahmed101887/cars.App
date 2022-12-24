c24b210129eb8321ae5922e23dc9695055aa3d69
Ahmed101887 <120632277+Ahmed101887@users.noreply.github.com>
2022-12-24 08:13:54 +0200

    .

--- lib/pages/checkout.dart
+++ lib/pages/checkout.dart
@@ -1,3 +1,4 @@
 / ignore_for_file: prefer_const_constructors
 
-import 'package:CARS.App/model/item.dart';
+import 'package:flower_app/model/item.dart';
+import 'package:flower_app/provider/cart.dart';
