20ab394846197e4014db9aaa576fb68771b0d764
Ahmed101887 <120632277+Ahmed101887@users.noreply.github.com>
2022-12-24 08:34:03 +0200

    /

--- lib/pages/checkout.dart
+++ lib/pages/checkout.dart
@@ -43,3 +43,6 @@ class CheckOut extends StatelessWidget {
                             },
                             icon: Icon(Icons.remove)),
                       ),
+                    );
+                  }),
+            ),
