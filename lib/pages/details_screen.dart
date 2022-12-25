f53da6626fb5e8aafaef77f933a7f7696e4a02c9
Ahmed101887 <120632277+Ahmed101887@users.noreply.github.com>
2022-12-25 10:03:08 +0200

    .

--- lib/pages/details_screen.dart
+++ lib/pages/details_screen.dart
@@ -78,3 +78,5 @@ class _DetailsState extends State<Details> {
                       Icon(
                         Icons.star,
                         size: 26,
+                        color: Color.fromARGB(255, 255, 191, 0),
+                      ),
