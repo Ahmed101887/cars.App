3608163d92b6b734d1b3742c9c93df933c603dfa
Ahmed101887 <120632277+Ahmed101887@users.noreply.github.com>
2022-12-25 09:51:28 +0200

    .

--- lib/pages/details_screen.dart
+++ lib/pages/details_screen.dart
@@ -4,3 +4,5 @@ import 'package:flower_app/model/item.dart';
 import 'package:flower_app/shared/appbar.dart';
 import 'package:flower_app/shared/colors.dart';
 import 'package:flutter/material.dart';
+
+class Details extends StatefulWidget {
