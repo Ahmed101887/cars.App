d6916cbd129a9d084b0223ce3de1a07b4fcd9c1f
Ahmed101887 <120632277+Ahmed101887@users.noreply.github.com>
2022-12-25 10:51:09 +0200

    ,

--- lib/pages/details_screen.dart
+++ lib/pages/details_screen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, must_be_immutable
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, must_be_immutable, sort_child_properties_last
 
 import 'package:flutter/material.dart';
 import 'package:flutter_application/model/item.dart';
