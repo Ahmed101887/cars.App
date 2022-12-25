3f8077fff586c9a08f2f849b843ba0097163e8f1
Ahmed101887 <120632277+Ahmed101887@users.noreply.github.com>
2022-12-25 09:50:57 +0200

    ,

--- lib/pages/details_screen.dart
+++ lib/pages/details_screen.dart
@@ -1,3 +1,4 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flower_app/model/item.dart';
+import 'package:flower_app/shared/appbar.dart';
